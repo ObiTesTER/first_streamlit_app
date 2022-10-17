79ed9cd8284c9b64bfec64648ca387bfc133e515
ObiTesTER <115453396+ObiTesTER@users.noreply.github.com>
2022-10-17 09:48:15 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,13 +25,12 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
+streamlit.header("Fruityvice Fruit Advice!")
 
 #New section to display FruityVice reponses
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "BANANA")
 
-
-streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+fruit_choice = streamlit.text_input('What fruit would you like information about?','BANANA')
 streamlit.write('The user entered ', fruit_choice)
 
 # write your own comment -what does the next line do? 
